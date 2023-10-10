44ad6252fa892032191e903776d5517504b8d86a
zhOhh <1299410261@qq.com>
2023-10-10 17:09:18 +0800

    AVTree

--- Code/Java/src/main/com/zh/datastructure/avltree/AVLTree.java
+++ Code/Java/src/main/com/zh/datastructure/avltree/AVLTree.java
@@ -1,6 +1,7 @@
 package main.com.zh.datastructure.avltree;
 
 public class AVLTree {
+
     static class AVLNode {
         int key;
         Object value;
@@ -32,6 +33,139 @@ public class AVLTree {
     }
 
     //更新节点高度
+    private void updateHeight(AVLNode node) {
+        node.height = Integer.max(height(node.left), height(node.right)) + 1;
+    }
 
+    //平衡因子（balance factor）= 左子树高度 - 右子树高度
+    public int bf(AVLNode node) {
+        return height(node.left) - height(node.right);
+    }
+
+    /**
+     * 右旋
+     *
+     * @param red 要旋转的节点
+     * @return 新的根节点
+     */
+    private AVLNode rightRotate(AVLNode red) {
+        AVLNode yellow = red.left;
+        AVLNode green = yellow.right;
+        yellow.right = red;
+        red.left = green;
+        updateHeight(red);
+        updateHeight(yellow);
+        return yellow;
+    }
+
+    /**
+     * 左旋
+     *
+     * @param red 要旋转的节点
+     * @return 新的根节点
+     */
+    private AVLNode leftRotate(AVLNode red) {
+        AVLNode yellow = red.right;
+        AVLNode green = yellow.left;
+        yellow.left = red;
+        red.right = green;
+        updateHeight(red);
+        updateHeight(yellow);
+        return yellow;
+    }
+
+    //先左旋左子树，再右旋根节点
+    private AVLNode leftRightRotate(AVLNode node) {
+        node.left = leftRotate(node.left);
+        return rightRotate(node);
+    }
+
+    //先右旋右子树，再左旋根节点
+    private AVLNode rightLeftRotate(AVLNode node) {
+        node.right = rightRotate(node.right);
+        return leftRotate(node);
+    }
+
+    //检查是否失衡，重新平衡代码
+    private AVLNode balance(AVLNode node) {
+        if (node == null) {
+            return null;
+        }
+        int bf = bf(node);
+        if (bf > 1 && bf(node.left) >= 0) {//LL
+            return leftRotate(node);
+        } else if (bf > 1 && bf(node.left) < 0) {//LR
+            return leftRightRotate(node);
+        } else if (bf < -1 && bf(node.right) > 0) {//RL
+            return rightLeftRotate(node);
+        } else if (bf < -1 && bf(node.right) <= 0) {//RR
+            return rightRotate(node);
+        }
+        return node;
+    }
+
+    AVLNode root;
+
+    public void put(int key, Object value) {
+        root = doPut(root, key, value);
+    }
+
+    private AVLNode doPut(AVLNode node, int key, Object value) {
+        //1.找到空位，创建新节点
+        if (node == null) {
+            return new AVLNode(key, value);
+        }
+        //2.key已存在，更新
+        if (key == node.key) {
+            node.value = value;
+            return node;
+        }
+        //3.继续查找
+        if (key < node.key) {
+            node.left = doPut(node.left, key, value);
+        } else {
+            node.right = doPut(node.right, key, value);
+        }
+        updateHeight(node);
+        return balance(node);
+    }
+
+    public void remove(int key) {
+        root = doRemove(root, key);
+    }
+
+    private AVLNode doRemove(AVLNode node, int key) {
+        //1.node==null
+        if (node == null) {
+            return null;
+        }
+        //2.没找到key
+        if (key < node.key) {
+            node.left = doRemove(node.left, key);
+        } else if (node.key < key) {
+            node.right = doRemove(node.right, key);
+        } else {
+            //3.找到key 1)没有孩子 2)只有一个孩子 3)有两个孩子
+            if (node.left == null && node.right == null) {
+                return null;
+            } else if (node.left == null) {
+                node = node.right;
+            } else if (node.right == null) {
+                node = node.left;
+            } else {
+                AVLNode s = node.right;
+                while (s.left != null) {
+                    s = s.left;
+                }
+                s.right = doRemove(node.right, s.key);
+                s.left = node.left;
+                node = s;
+            }
+        }
+        //4.更新高度
+        updateHeight(node);
+        //5.balance
+        return balance(node);
+    }
 
 }
